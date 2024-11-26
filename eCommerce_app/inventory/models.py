35bc71a952e63d7770a0e8707c531b7f23c476e9
Daniel Mulumbi <139741946+DanKas3009@users.noreply.github.com>
2024-11-26 17:17:43 +0300

    Making models for each app

--- eCommerce_app/inventory/models.py
+++ eCommerce_app/inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class inventory(models.Model):
+    inventoryID = models.AutoField(primary_key=True)
+    ProductID = models.ForeignKey('Product', on_delete=models.CASCADE)
+    QuantityInStock = models.IntegerField()
+    ReorderLevel = models.IntegerField()
+    LastRestockDate = models.DateField()
+    Location = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.inventoryID
